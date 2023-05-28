fc9382c87f7cfedf68fa28852ed0452d9830383e
vagap <vagapov.et@phystech.edu>
2023-05-28 20:24:55 +0300

    Add Idempotency

--- src/main/java/edu/mipt/accounts/impl/IdempotencyAccounts.java
+++ src/main/java/edu/mipt/accounts/impl/IdempotencyAccounts.java
@@ -3,9 +3,12 @@ package edu.mipt.accounts.impl;
 import edu.mipt.accounts.AccountResponse;
 import edu.mipt.accounts.Accounts;
 import lombok.RequiredArgsConstructor;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 import static edu.mipt.accounts.AccountResponse.okResponse;
@@ -13,17 +16,28 @@ import static edu.mipt.accounts.AccountResponse.okResponse;
 @Service
 @Transactional(noRollbackFor = AccountException.class)
 @RequiredArgsConstructor
+@Retryable(maxAttempts = 20)
 public class IdempotencyAccounts implements Accounts {
+    private Map<String, AccountResponse> rqUidToResponse = new HashMap<>();
     private final AccountRepository accountRepository;
 
     @Override
     public AccountResponse withdraw(String rqUid, long accountId, long amount) {
-        return process(accountId, acc -> acc.withdraw(amount));
+        return checkAndAddResponseInMap(rqUid, accountId, acc -> acc.withdraw(amount));
     }
 
     @Override
     public AccountResponse deposit(String rqUid, long accountId, long amount) {
-        return process(accountId, acc -> acc.deposit(amount));
+        return checkAndAddResponseInMap(rqUid, accountId, acc -> acc.deposit(amount));
+    }
+
+    private AccountResponse checkAndAddResponseInMap(String rqUid, long accountId, Consumer<Account> processing) {
+        if (!rqUidToResponse.containsKey(rqUid)) {
+            AccountResponse accountResponse = process(accountId, processing);
+            rqUidToResponse.put(rqUid, accountResponse);
+            return accountResponse;
+        }
+        return rqUidToResponse.get(rqUid);
     }
 
     private AccountResponse process(long accountId, Consumer<Account> processing) {
